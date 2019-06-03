11188c69f4e7c3f4631bb825de1435bd214b01ed
Fanqiang Meng <fanmeng0417@gmail.com>
2019-06-03 01:00:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,5 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get(/name){"My name is Fan"}
   get(/hometown){"My hometown is Lanzhou"}
+  get(/favorite-song){"My favorite song is No idea"}
 end
